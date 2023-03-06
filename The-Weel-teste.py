8a0c0798c9b8dca8daae2f8932e1476c1d42b016
ErickCassoli <erick.mcassoli@gmail.com>
2023-03-06 17:28:40 -0300

    subindo a 3.0 e alguns testes

--- The-Weel-teste.py
+++ The-Weel-teste.py
@@ -5,6 +5,8 @@ def linha():
     print('\n\033[1;35m-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
 
 info = []
+num = 1
+chrs = ''
 
 print('\n\033[1;35m------------------------------------------\n\n\033[1;31m T H 3  W 3 3 L \033[32m2.0\033[1;31m| WORDLIST GENERATOR\n\n\033[1;96m~ by: Ydr0l\n\n\033[1;35m------------------------------------------\n')
 
@@ -16,40 +18,45 @@ linha()
 
 personal_list = str(input("\n\033[1;92m[?] Quer criar uma lista pessoal ? [Y/N] [?] : ")).upper()
 
-if personal_list == 'Y':
-    while True:
-        str_info = input(f"\n\033[1;96m[*] Informação {len(info)+1} : ")
-        info.append(str_info)
+linha()
 
+if personal_list == 'Y':
+    
+    moreinfo = 'Y'
+
+    for num in itertools.count(start=1):
+        str_info = input(f"\n\033[1;96m[*] Informação {num} : ")
+        info.append(str_info)
+        
         linha()
 
         moreinfo = str(input("\n\033[1;92m[*] Quer acrescentar mais informação? [Y/N] [*] : ")).upper()
 
         linha()
-
+        
         if moreinfo != 'Y':
             break
 
-info_str = ''.join(info)
-chars = []
-chars_str = ''
+for inf in info :
+    chrs = ''.join([chrs, inf])
 
-chars.extend(list(info_str))
-if str(input('\n\033[36m[?] Quer usar o alfabeto? [Y/N] [?] : ')).upper() == 'Y':
-    chars.extend(list('abcdefghijklmnopqrstuvwxyz'))
-    chars_str = ''.join(chars)
-if str(input('\n\033[36m[?] Quer usar caracteres maiusculos? [Y/N] [?] : ')).upper() == 'Y':
-    chars.extend(list(info_str.upper()))
-    if chars_str == '':
-        chars.extend(list(chars_str.upper()))
-if str(input('\n\033[36m[?] Quer usar caracteres especiais? [Y/N] [?] : ')).upper() == 'Y':
-    chars.extend(list('!\][/?.,~-=";:><@#$%&*()_+\' '))
-if str(input('\n\033[36m[?] Quer usar caracteres numericos? [Y/N] [?] : ')).upper() == 'Y':
-    chars.extend(list('1234567890'))
-repeat_chars = str(input('\n\033[36m[?] Quer permitir caracteres repetidos? [Y/N] [?] : ')).upper()
+chrs_alfabetic = 'abcdefghijklmnopqrstuvwxyz'
+chrs_up = chrs.upper()
+chrs_specials = '!\][/?.,~-=";:><@#$%&*()_+\' '
+chrs_numerics = '1234567890'
 
 file_name = input('\n\033[1;91m[!] Qual o nome da sua wordlist? [!] : ')
-arq = open(file_name+".txt", 'w')
+arq = open(file_name, 'w')
+if str(input('\n\033[36m[?] Quer usar o alfabeto? [Y/N] [?] : ')).upper() == 'Y':
+    chrs = ''.join([chrs, chrs_alfabetic])
+if str(input('\n\033[36m[?] Quer usar caracteres maiusculos? [Y/N] [?] : ')).upper() == 'Y':
+    chrs = ''.join([chrs, chrs_up])
+if str(input('\n\033[36m[?] Quer usar caracteres especiais? [Y/N] [?] : ')).upper() == 'Y':
+    chrs = ''.join([chrs, chrs_specials])
+if str(input('\n\033[36m[?] Quer usar caracteres numericos? [Y/N] [?] : ')).upper() == 'Y':
+    chrs = ''.join([chrs, chrs_numerics])
+
+repeat_chars = str(input('\n\033[36m[?] Quer permitir caracteres repetidos? [Y/N] [?] : ')).upper()
 
 then = time.time()
 
@@ -58,13 +65,10 @@ print("\n\033[1;32m [!] Gerando a wordlist [!]\n")
 used_combinations = set()
 
 for i in range(start, final+1):
-    for j in itertools.product(chars, repeat=i):
+    for j in itertools.product(chrs, repeat=i):
         temp = ''.join(j)
-        if repeat_chars == 'N':
-            if not any(c in temp for c in used_combinations) and len(set(temp)) == len(temp):
-                used_combinations.add(temp)
-                arq.write(temp + '\n')
-        else:
+        if not any(c in temp for c in used_combinations):
+            used_combinations.add(temp)
             arq.write(temp + '\n')
 
 arq.close()
